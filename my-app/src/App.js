32ee58782c3b91b1b829efae3b4a64dff3094dfb
marekkulesza <66383506+marekkulesza@users.noreply.github.com>
2023-01-15 20:36:59 -0500

    Fixed some node modules discrepancies

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf';
 import './App.css';
 import MyForm from './form.js';
 
 // how to start - Marek's Notes
 // cd '.\COMPCO867 Software Engineering Project\my-app' 
 // INSTALL THIS IN THE ROOT FOLDER OF THE PROJECT
-// npm install @react-pdf/renderer --save
-
-
+// npm install react-pdf
 // npm start
 
 
 function App() {
+
+  const url = 'localhost:3000/formPDF.pdf'
+
+  const [pdfRef, setPdfRef] = useState(null);
+
+  const downloadPDF = () => {
+    if (pdfRef) {
+        pdfRef.current.save();
+    }
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
         <h1>Registration Form</h1>
-        <div>
-          <MyForm />
-        </div>
-
       </header>
+
+      <button onClick={downloadPDF}>Download PDF</button>
+      <Document file={url} ref={setPdfRef}>
+        <Page>
+          <MyForm />
+        </Page>
+      </Document>
+
     </div>
   );
 }

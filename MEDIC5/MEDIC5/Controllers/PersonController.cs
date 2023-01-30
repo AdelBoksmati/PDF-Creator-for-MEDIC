938f82dbd4f5d19e34b3b9eae58adaf7e9491e57
marekkulesza <66383506+marekkulesza@users.noreply.github.com>
2023-01-29 22:29:01 -0500

    Started working on learning how the text should work.

--- MEDIC5/MEDIC5/Controllers/PersonController.cs
+++ MEDIC5/MEDIC5/Controllers/PersonController.cs
@@ -22,30 +22,26 @@ namespace MEDIC5.Controllers
         [HttpPost(Name = "CheckDetails")]
         public ActionResult Details([FromBody] Person person)
         {
+
+            // TODO: Do a better job at validating the model
             if (person == null || string.IsNullOrEmpty(person.Name) || string.IsNullOrEmpty(person.Email) || string.IsNullOrEmpty(person.Password) || string.IsNullOrEmpty(person.ConfirmPassword) || string.IsNullOrEmpty(person.PhoneNumber) || string.IsNullOrEmpty(person.Address) || string.IsNullOrEmpty(person.City) || string.IsNullOrEmpty(person.Province) || string.IsNullOrEmpty(person.ZipCode) || string.IsNullOrEmpty(person.Country) || string.IsNullOrEmpty(person.DateOfBirth))
             {
                 return BadRequest("Missing required fields");
             }
-            // TODO: Do a better job at validating the model
 
+            // This is the file creation part.
             var filePath = "PersonDocument.pdf";
 
+            // This then creates the PersonDocument passing the person object that came through the validation
             var document = new PersonDoucument(person);
             document.GeneratePdf(filePath);
 
+            // Opens the PDF
             Process.Start("explorer.exe", filePath);
 
             return Ok(new { message = "This worked" });
 
-
-
-
-
         }
-
-        
-
-
 
     }
 }
--- MEDIC5/MEDIC5/Documents/PersonDoucument.cs
+++ MEDIC5/MEDIC5/Documents/PersonDoucument.cs
@@ -24,10 +24,46 @@ namespace MEDIC5.Documents
                 {
                     page.Margin(50);
 
-                    page.Header().Height(100).Background(Colors.Grey.Lighten1);
-                    page.Content().Background(Colors.Grey.Lighten3);
-                    page.Footer().Height(50).Background(Colors.Grey.Lighten1);
+                    page.Header().Element(ComposeHeader);
+                    page.Content().Element(ComposeContent);
+
+
+                    page.Footer().AlignCenter().Text(x =>
+                    {
+                        x.CurrentPageNumber();
+                        x.Span(" / ");
+                        x.TotalPages();
+                    });
                 });
+        }
+
+        void ComposeHeader(IContainer container)
+        {
+            container.Background(Colors.Grey.Lighten3).Padding(10).Column(column =>
+            {
+
+                column.Spacing(5);
+                column.Item().Text("RCM Health Consultancy Inc.").FontSize(14);
+                column.Item().Text("1006 Avenue Road, Suite 100, Toronto, Ontario, M5P 2K8 Canada").FontSize(12);
+                column.Item().Text("(647) 350-5500, (416) 961-3322, info@rcmhealth.ca").FontSize(12);
+            
+                column.Item()
+                        .Width(100)
+                        .Height(100)
+                        .Image("Logo.png");
+
+            });
+        }
+
+        void ComposeContent(IContainer container)
+        {
+            container
+                .PaddingVertical(40)
+                .Height(250)
+                .Background(Colors.Grey.Lighten3)
+                .AlignCenter()
+                .AlignMiddle()
+                .Text("Content").FontSize(16);
         }
 
 
